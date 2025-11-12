e21cb324ecdf1a7cabe0f01a3863bab6c79fa17c
Akhand Pratap Singh <singhakhands164@gmail.com>
2025-11-13 00:05:33 +0530

    feat: add CORS support, implement signup functionality, and enhance UI components

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from  'express';
 import cookieParser from 'cookie-parser';
 import { ENV } from './lib/env.js';
 import path from 'path';
-
+import cors from 'cors';
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
 import { connectDB } from './lib/db.js';
@@ -13,6 +13,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT;
 
 app.use(express.json());
+app.use(cors({origin:ENV.CLIENT_URL, credentials:true}));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
